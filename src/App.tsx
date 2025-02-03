f0b624b54c1eadbfee6514bd10899ef2f794a3c7
ruchit2106 <87220033+ruchit2106@users.noreply.github.com>
2025-02-03 22:13:12 +0530

    Testing for Build

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material";
 const App = () => {
   return (
     <div className="App">
-      <Typography variant="h4" align="center">🔥HAIL REVOLUTION🔥</Typography>
+      <Typography variant="h4" align="center">🔥LONG LIVE THE REVOLUTION🔥</Typography>
     </div>
   );
 };
